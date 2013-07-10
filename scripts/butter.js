6b2af94d89a5c4bb98e012b63222b083af08756e
Paige Ponzeka <pponzeka@yext.com>
2013-07-10 16:53:06 -0400

    Making it a little smarter and cleaning up some styling

--- scripts/butter.js
+++ scripts/butter.js
@@ -14,19 +14,16 @@
 
 	// Button handler for adding new recipe
 	$addRecipeButton.click(function(e){
-		console.log("Adding RECIPE");
 		$recipeContainer.append(recipeHtml);
 	});
 
 	// Button handler for adding new ingredient (need to use on)
 	$('#recipes').on('click', addIngredientButtons, function(e){
-		// TODO - need to make this smarter to handle appending to the actual parent when there is more than one recipe card.
-		$(ingredientContainer).append(ingredientHtml);
+		$(this).parent('.recipe').find(ingredientContainer).append(ingredientHtml);
 	});
 
 	function init(){
 		//adds a blank recipe and a blank ingredient
-
 		$addRecipeButton.click();
 		$(addIngredientButtons).click();
 	}
