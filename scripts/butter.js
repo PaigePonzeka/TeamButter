c3690808789b22c25b31f26d523a96db6e49f0b4
Paige Ponzeka <pponzeka@yext.com>
2013-07-11 11:11:55 -0400

    Adding a testing to generate recipes and ingredients, still no shopping list functionality yet

--- scripts/butter.js
+++ scripts/butter.js
@@ -12,14 +12,22 @@
 	var $addRecipeButton = $('#addRecipe');
 	var addIngredientButtons = '.js-add-ingredient'; // not an object because they are generated per recipe
 
+	var test_items = ["garlic", "orange", "sugar", "Garlic", "Sugar", "brown sugar"];
+
+	var measurementOptions = ["whole", "cup", "teaspoon", "tablespoon", "can"];
+	// TODO use handlebars to generate measurement Options for select box
 	// Button handler for adding new recipe
 	$addRecipeButton.click(function(e){
 		$recipeContainer.append(recipeHtml);
 	});
 
 	// Button handler for adding new ingredient (need to use on)
-	$('#recipes').on('click', addIngredientButtons, function(e){
+	$recipeContainer.on('click', addIngredientButtons, function(e){
 		$(this).parent('.recipe').find(ingredientContainer).append(ingredientHtml);
+	});
+
+	$('#runTest').click(function(){
+		testApplication();
 	});
 
 	function init(){
@@ -30,4 +38,27 @@
 
 	// intializing the application
 	init();
+
+
+	function testApplication(){
+		var recipesList = $recipeContainer.find('.recipe');
+
+		// for each recipe generate a name
+		$(recipesList).each(function(index){
+			$(this).find('.recipe-name').val("Test Recipe Name " + (index + 1));
+			// for each ingredient in each recipe generate an ingredient
+			var ingredientList = $(this).find(ingredientContainer);
+			$(ingredientList).each(function(index){
+				$(this).find('.recipe-amount').val(1);
+				$('.recipe-measurement').val(measurementOptions[3]);
+				$(this).find('.recipe-item').val(test_items[randomInt(0, (test_items.length-1))]);
+			});
+		}); 
+	}
+
+	/* UTILITIES */
+
+	function randomInt(start, end){
+		return start + Math.floor(Math.random() * end);
+	}
 })($);
